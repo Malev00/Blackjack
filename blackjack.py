f0e3332a3beebca545be004e57fdb0cdf496e2a0
Malev00 <malev00@icloud.com>
2023-10-24 10:48:20 +0300

    add decks

--- blackjack.py
+++ blackjack.py
@@ -357,4 +357,22 @@ if __name__ == '__main__':
     cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
     # The card value
+    cards_values = {"A": 11, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10}
+
+    # The deck of cards
+    deck = []
+
+    # Loop for every type of suit
+    for suit in suits:
+
+        # Loop for every type of cards in a suit
+        for card in cards:
+
+            # Adding card to the deck
+            deck.append(Card(suits_value[suit], card, cards_values[card]))
+
+    blackjack_game(deck)
+
+    
+    
     
